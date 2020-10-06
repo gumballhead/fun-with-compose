6075cd12c3e2da4fb973ee00049d666c36f78c89
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2020-10-06 12:25:24 -0500

    Update some styles

--- app/src/main/java/com/example/cleankt/counter/CounterScreen.kt
+++ app/src/main/java/com/example/cleankt/counter/CounterScreen.kt
@@ -1,10 +1,13 @@
 package com.example.cleankt.counter
 
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
@@ -15,17 +18,25 @@ interface Counter {
 
 @Composable
 fun Count(count: Int) {
-    Text("$count")
+    Text("$count",
+        fontSize = TextUnit.Companion.Sp(36)
+    )
 }
 
 @Composable
 fun CounterScreen(count: Int, counter: Counter) {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Count(count)
+        Spacer(Modifier.preferredHeight(16.dp ))
 
         Row {
-            Button(onClick=counter::onDecrement) { Text("-") }
-            Button(onClick=counter::onIncrement) { Text("+") }
+            Button(onClick=counter::onDecrement) {
+                Text("-")
+            }
+
+            Button(onClick=counter::onIncrement) {
+                Text("+")
+            }
         }
     }
 }
@@ -34,5 +45,5 @@ fun CounterScreen(count: Int, counter: Counter) {
 @Composable
 @ExperimentalCoroutinesApi
 fun PreviewCounter() {
-    CounterScreen(count = 10, counter = CountInteractor())
+    CounterScreen(count=10, counter=CountInteractor())
 }
